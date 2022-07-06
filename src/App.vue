9d17db1a22b3c4a7f59ddeb0a9122e5f42ba391f
fissart <ricardomallqui6@gmail.com>
2022-07-06 13:42:46 -0500

    Initial comment

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,44 @@
 <template>
   <img alt="Vue logo" class="imgw" src="./assets/negzw.jpg">
+  <tr v-for="wwwww in www" :value="wwwww._id" :key="wwwww._id">
+  <td>{{wwwww._id}}</td>
+  <td>{{wwwww.title}}</td>
+    <td>{{wwwww.user}}</td>
+  </tr>
+
   <HelloWorld msg="wwWelcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+
+  data(){
+  return {
+  www:null
   }
+  },
+
+  mounted(){
+this.getWw();
+  },
+
+  methods:{
+     getWw(){
+     axios.get("http://169.197.183.233:3000/api/curses/Controller")
+     .then(responce => this.www=responce.data)
+     .catch(e=>console.log(e))
+    }
+
+  }
+
+
 }
 </script>
 
@@ -26,6 +54,7 @@ export default {
 .imgw {
 width:50%;
   margin-top: 60px;
+  background-color: gray
 }
 
 </style>
---
+++ src/components/HelloWorld3.vue
@@ -0,0 +1,13 @@
+<template>
+<div class="www">
+
+</div>
+</template>
+<script>
+
+</script>
+<style>
+  .www{
+    background-image: url("./assets/logo.png");
+  }
+</style>
